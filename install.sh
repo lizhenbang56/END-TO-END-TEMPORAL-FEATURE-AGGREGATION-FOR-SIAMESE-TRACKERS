83186353bc263fb56357a676f2aa0db77f4d11bd
ShiYaya <15855143837@163.com>
2021-05-17 09:27:27 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-source activate pytorch1.4_python3.7
+# source activate pytorch1.4_python3.7  # etvuz@172.18.32.31
+source activate py37-pytorch1.4  # yyshi@172.18.32.157
 export PATH=/usr/local/cuda-10.1/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64
 python setup.py build develop
